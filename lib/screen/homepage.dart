6a301b0b01b8b67b4570e85fd5c908fd06dcd3ca
Muhammad Afif <mr.qwertyuiop981224@gmail.com>
2022-06-05 19:15:34 +0800

    Setup

---
+++ lib/screen/homepage.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
